b1646abf769c60bae9b5a0d65e6110df3fb7bbb7
Yoon-Min <yoonmin0113@naver.com>
2023-06-29 20:04:33 +0900

    [ADD]: 필요한 의존성 추가 및 네트워크 통신 관련 인터셉트 추가

--- app/src/main/java/com/example/refit/data/network/NetworkInterceptor.kt
+++ app/src/main/java/com/example/refit/data/network/NetworkInterceptor.kt
@@ -1,4 +1,11 @@
 package com.example.refit.data.network
 
-class NetworkInterceptor {
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class NetworkInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder().addHeader("Content-Type", "application/json")
+        return chain.proceed(builder.build())
+    }
 }
