39e97c084222e0ebae637875ac897120c85e5351
Yoon-Min <yoonmin0113@naver.com>
2023-07-01 19:44:24 +0900

    [UI] UI/UX 디자인에 따른 바텀 네비게이션 추가 설정 진행

--- app/src/main/java/com/example/refit/MainActivity.kt
+++ app/src/main/java/com/example/refit/MainActivity.kt
@@ -8,30 +8,44 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.refit.databinding.ActivityMainBinding
 import com.example.refit.presentation.common.BaseActivity
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.bnvMain.setupWithNavController(findNavController())
-        setNavigationBarVisibility()
+        binding.bnvMain.setupWithNavController(getNavController())
+        handleNavigationBarVisibility()
+
+        initBottomNavigationBackground()
     }
 
-    private fun findNavController(): NavController {
+    private fun getNavController(): NavController {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
 
         return navHostFragment.navController
     }
 
-    private fun setNavigationBarVisibility() {
-        findNavController().addOnDestinationChangedListener { _, destination, _ ->
+    private fun handleNavigationBarVisibility() {
+        getNavController().addOnDestinationChangedListener { _, destination, _ ->
             binding.bnvMain.visibility =
                 when (destination.id) {
                     R.id.nav_community, R.id.nav_my_page, R.id.nav_closet -> View.VISIBLE
                     else -> View.GONE
                 }
         }
+    }
+
+    private fun initBottomNavigationBackground() {
+        val radius = resources.getDimension(R.dimen.big_radius)
+        val bottomNavBackground: MaterialShapeDrawable = binding.bnvMain.background as MaterialShapeDrawable
+        bottomNavBackground.shapeAppearanceModel = bottomNavBackground.shapeAppearanceModel
+            .toBuilder()
+            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+            .setTopRightCorner(CornerFamily.ROUNDED, radius)
+            .build()
     }
 }
