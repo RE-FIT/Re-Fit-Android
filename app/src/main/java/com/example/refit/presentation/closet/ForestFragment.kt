11d384c1cd984073587874bb25bcf7f9d11c4c17
Yoon-Min <yoonmin0113@naver.com>
2023-07-24 01:23:41 +0900

    [ADD] 숲 메인 화면 레이아웃 추가

---
+++ app/src/main/java/com/example/refit/presentation/closet/ForestFragment.kt
@@ -0,0 +1,19 @@
+package com.example.refit.presentation.closet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.refit.R
+import com.example.refit.databinding.FragmentForestBinding
+import com.example.refit.presentation.common.BaseFragment
+
+class ForestFragment : BaseFragment<FragmentForestBinding>(R.layout.fragment_forest) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
