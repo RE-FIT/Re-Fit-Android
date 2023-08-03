635442ebef4eb25655529aa7d489aba8c42e92e9
hyunal7131 <hyunal7131@gmail.com>
2023-08-03 22:00:56 +0900

    [FIX]로그인 회원가입 아이디비번찾기

--- app/src/main/java/com/example/refit/presentation/signin/SignInFragment.kt
+++ app/src/main/java/com/example/refit/presentation/signin/SignInFragment.kt
@@ -35,8 +35,13 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
         //id: admin1234
         //password: AAaa1234!!
         binding.signInExistingLogin.setOnClickListener {
-            // vm.signUpUser("admin1234", "AAaa1234!!","refit@gmail.com","어드민","2023/07/12", 0)
-            viewModel.basicLogin("admin1234", "AAaa1234!!")
+            val id = binding.signInLoginId.toString()
+            val password = binding.signInFindIdPassword.toString()
+
+            /*vm.signUpUser("admin1234", "AAaa1234!!","refit@gmail.com","어드민","2023/07/12", 0)*/
+            //viewModel.basicLogin("admin1234", "AAaa1234!!")
+            Timber.d("$id $password")
+            viewModel.basicLogin(id, password)
         }
 
 
