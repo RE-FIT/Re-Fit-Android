343f9d2ff07f896a26d85fca66ead969edea3d14
Ji Seungmin <cswcsm02@gmail.com>
2023-08-11 22:33:10 +0900

    [feat]: 로그아웃 기능 및 로그인 기능 구현

--- app/src/main/java/com/example/refit/presentation/signin/SignInFragment.kt
+++ app/src/main/java/com/example/refit/presentation/signin/SignInFragment.kt
@@ -9,7 +9,7 @@ import com.example.refit.presentation.common.BaseFragment
 import com.example.refit.presentation.common.CustomSnackBar
 import com.example.refit.presentation.common.NavigationUtil.navigate
 import com.example.refit.presentation.signin.viewmodel.SignInViewModel
-import com.example.refit.presentation.signup.viewmodel.SignUpViewModel
+import com.example.refit.util.EventObserver
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import timber.log.Timber
 
@@ -36,8 +36,7 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
             }
         }
 
-        viewModel.error.observe(viewLifecycleOwner) {
-
+        viewModel.error.observe(viewLifecycleOwner, EventObserver{
             val customSnackBar = CustomSnackBar.make(
                 view = requireView(),
                 layout = R.layout.custom_snackbar_sign_fail,
@@ -47,7 +46,7 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
             customSnackBar.setTitle("존재하지 않는 계정입니다.", "아이디 또는 비밀번호를 다시 한 번 확인해주세요!")
 
             customSnackBar.show()
-        }
+        })
 
         //로그인
         binding.signInExistingLogin.setOnClickListener {
@@ -59,9 +58,9 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
 
 
         //로그인 시 이동
-        viewModel.accessToken.observe(viewLifecycleOwner) {
+        viewModel.accessToken.observe(viewLifecycleOwner, EventObserver {
             navigate(R.id.action_signInFragment_to_communityFragment)
-        }
+        })
 
         //회원가입 이동
         binding.join.setOnClickListener(){
