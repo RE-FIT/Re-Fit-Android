c1dc9cc2f4df80a74efbd6c72159a5ffb8ffc0d0
Ji Seungmin <cswcsm02@gmail.com>
2023-08-12 21:09:27 +0900

    [feat]: notification 기능 추가

--- app/src/main/java/com/example/refit/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/refit/MyFirebaseMessagingService.kt
@@ -1,5 +1,13 @@
 package com.example.refit
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -10,5 +18,52 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        // 알림 내용을 가져옴
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+
+        title?.let { t ->
+            body?.let { b ->
+                showNotification(t, b)
+            }
+        }
+    }
+
+    private fun showNotification(title: String, messageBody: String) {
+
+        //MainActivity를 최상단으로 이동
+        val intent = Intent(this, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+
+        //
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = "refit"
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.refit)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        //알림을 표시하기 위해 Manager를 가지고 옴
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "refit",
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        //동일한 알람이 여러번 왔을때 이전 알람을 대체
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
