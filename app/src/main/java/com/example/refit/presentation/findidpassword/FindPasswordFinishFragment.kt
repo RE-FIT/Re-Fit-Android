c468f4d43ff25d1f7a2bb87958e0989420e552f7
Ji Seungmin <cswcsm02@gmail.com>
2023-08-19 01:43:47 +0900

    [fix]: ChatFragment 다이얼로그 적용 및 비밀번호 찾기 후 화면 초기화하도록 수정

--- app/src/main/java/com/example/refit/presentation/findidpassword/FindPasswordFinishFragment.kt
+++ app/src/main/java/com/example/refit/presentation/findidpassword/FindPasswordFinishFragment.kt
@@ -1,9 +1,11 @@
 package com.example.refit.presentation.findidpassword
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.navigation.fragment.navArgs
+import com.example.refit.MainActivity
 import com.example.refit.R
 import com.example.refit.databinding.FragmentFindPasswordFinishBinding
 import com.example.refit.presentation.common.BaseFragment
@@ -17,8 +19,9 @@ class FindPasswordFinishFragment : BaseFragment<FragmentFindPasswordFinishBindin
         binding.findPasswordFinishEmail.text = args.email
 
         binding.findPasswordBtn.setOnClickListener(){
-            Toast.makeText(context, "이메일로 임시 비밀번호가 발급되었습니다.", Toast.LENGTH_LONG).show()
-            navigate(R.id.action_findPasswordFinishFragment_to_signInFragment)
+            requireActivity().finish()
+            val restartIntent = Intent(context, MainActivity::class.java)
+            startActivity(restartIntent)
         }
     }
 }
