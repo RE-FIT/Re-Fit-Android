1d472bb3a981214f76fb2a34c6937359b17dc1b8
Ji Seungmin <cswcsm02@gmail.com>
2023-08-22 19:24:30 +0900

    [feat]: notificationStore Koin 의존성 추가

---
+++ app/src/main/java/com/example/refit/data/datastore/NotificationStore.kt
@@ -0,0 +1,7 @@
+package com.example.refit.data.datastore
+
+import androidx.datastore.core.DataStore
+
+class NotificationStore(private val dataStore: DataStore<Notifications>) {
+
+}
--- app/src/main/java/com/example/refit/di/DataStoreModule.kt
+++ app/src/main/java/com/example/refit/di/DataStoreModule.kt
@@ -1,16 +1,89 @@
 package com.example.refit.di
 
 import android.content.Context
+import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.example.refit.data.datastore.NotificationStore
+import com.example.refit.data.datastore.Notifications
 import com.example.refit.data.datastore.TokenStore
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import java.io.InputStream
+import java.io.OutputStream
+
+//val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_token")
+//
+//val dataStoreModule = module {
+//    single { androidContext().dataStore }
+//    single { TokenStore(get()) }
+//}
+//
+//object NotificationsSerializer : Serializer<Notifications> {
+//
+//    override val defaultValue: Notifications
+//        get() = Notifications.getDefaultInstance()
+//
+//    override suspend fun readFrom(input: InputStream): Notifications {
+//        try {
+//            return Notifications.parseFrom(input)
+//        } catch (exception: Exception) {
+//            throw CorruptionException("Cannot read proto.", exception)
+//        }
+//    }
+//
+//    override suspend fun writeTo(t: Notifications, output: OutputStream) {
+//        t.writeTo(output)
+//    }
+//}
+//
+//val Context.notificationStore: DataStore<Notifications> by dataStore(
+//    fileName = "notifications.pb",
+//    serializer = NotificationsSerializer
+//)
+//
+//val notificationStoreModule = module {
+//    single { androidContext().notificationStore }
+//    single { NotificationStore(get()) }
+//}
+
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_token")
 
+object NotificationsSerializer : Serializer<Notifications> {
+
+    override val defaultValue: Notifications
+        get() = Notifications.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): Notifications {
+        try {
+            return Notifications.parseFrom(input)
+        } catch (exception: Exception) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: Notifications, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
+
+val Context.notificationStore: DataStore<Notifications> by dataStore(
+    fileName = "notifications.pb",
+    serializer = NotificationsSerializer
+)
+
+val PreferenceDataStore = named("dateStore")
+val NotificationDataStore = named("notificationStore")
+
 val dataStoreModule = module {
-    single { androidContext().dataStore }
-    single { TokenStore(get()) }
+    single(PreferenceDataStore) { androidContext().dataStore }
+    single(NotificationDataStore) { androidContext().notificationStore }
+
+    single { TokenStore(get(PreferenceDataStore)) }
+    single { NotificationStore(get(NotificationDataStore)) }
 }
